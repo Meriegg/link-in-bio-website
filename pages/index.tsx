6abf5fec3f232da9ddfb27a3db83d7e24ac84069
Meriegg <ya.boybeats1234@gmail.com>
2022-07-08 00:48:32 +0300

    Resolverd bugs

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,11 +27,11 @@ const Home: NextPage = () => {
           className={styles.links_button_icon}
         >
           <path
-            fill-rule="evenodd"
+            fillRule="evenodd"
             d="M6.364 2.5a.5.5 0 0 1 .5-.5H13.5A1.5 1.5 0 0 1 15 3.5v10a1.5 1.5 0 0 1-1.5 1.5h-10A1.5 1.5 0 0 1 2 13.5V6.864a.5.5 0 1 1 1 0V13.5a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5v-10a.5.5 0 0 0-.5-.5H6.864a.5.5 0 0 1-.5-.5z"
           />
           <path
-            fill-rule="evenodd"
+            fillRule="evenodd"
             d="M11 10.5a.5.5 0 0 1-.5.5h-5a.5.5 0 0 1 0-1h3.793L1.146 1.854a.5.5 0 1 1 .708-.708L10 9.293V5.5a.5.5 0 0 1 1 0v5z"
           />
         </svg>
@@ -109,8 +109,9 @@ const Home: NextPage = () => {
         transition={{ staggerChildren: 0.2, delay: 0.5, duration: 0.2 }}
         className={styles.links}
       >
-        {ContactLinks.map((contactLink) => (
+        {ContactLinks.map((contactLink, index) => (
           <ContactLink
+            key={index}
             link={contactLink.link}
             svg={contactLink.svg}
             text={contactLink.text}
